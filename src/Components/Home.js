1a7a97937369f6ca21bf988e44318a2e76df2f87
Yimin Yang <yvonne.yangh@gmail.com>
2023-08-27 18:54:29 +0800

    log in show username
    log in show username

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,12 @@
 import React,{useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import LoginModal from './Login';
+import { useSelector } from 'react-redux';
 
 
 
 const Home = ()=> {
+  const user = useSelector((state) => state.auth)
   const navigate = useNavigate();
   const [isLoginModalOpen, setIsLoginModal] = useState(false)
 
@@ -18,11 +20,13 @@ const Home = ()=> {
     setIsLoginModal(true)
   }
 
+ 
   return (
     <div id="homeDiv" style={{background:'#cea573',height:'97.76vh',textAlign:"center"}}>
 
       <div id="homeContainer" style={{dispaly:"flex",alignItems:"center",flexDirection:"column",justifyContent:"center",textAlign:"center",marginLeft:"5rem",marginRight:"5rem"}}>
         <div style={{fontSize: "260px",textAlign:"center",paddingTop:"6rem"}}>☕️</div>
+        <p style={{color:"white",fontSize:"2rem"}}> welcome{ user.username?<span> {user.username} !</span>:<span style={{color:"white"}}> !</span>}</p>
         <div style={{textAlign:"center", paddingTop:"1rem",paddingBottom:"1rem"}}>
           <button style={{width:"10rem"}} onClick={handleStartGame}>Start Game</button>
         </div>
